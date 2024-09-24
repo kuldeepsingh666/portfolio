0dbaa4a41843cd3bfb3b28a1fdb2524884b393ef
Kuldeep <kuldeeps.official@gmail.com>
2024-09-24 23:17:21 +0530

    Updated details of cv

--- main.py
+++ main.py
@@ -1,83 +1,84 @@
 import streamlit as st
 
 # Title of the portfolio
-st.title('Kuldeep Singh - Software Developer | DevOps Engineer')
+st.title('KULDEEP SINGH - Software Developer | DevOps Engineer')
 
 st.write("""
-**Address:** 838 Kasba Peth, behind Maya Bakery Pune 411011  
-**Phone:** +91 7387236721  | **Email:** [kuldeeps.official@gmail.com](mailto:kuldeeps.official@gmail.com)  
+📍 **Address:** 838 Kasba Peth, Behind Maya Bakery, Pune 411011  
+📞 **Phone:** +91 7387236721  | ✉ **Email:** [kuldeeps.official@gmail.com](mailto:kuldeeps.official@gmail.com)  
 **LinkedIn:** [linkedin.com/in/kuldeepsingh666](https://www.linkedin.com/in/kuldeepsingh666/)  | **GitHub:** [github.com/kuldeepsingh666](https://github.com/kuldeepsingh666)
 """)
 
 # Technical Skills
-st.header('Technical Skills')
+st.header('TECHNICAL SKILLS')
 st.write("""
-- **Languages:** Python, Go, HTML, Java, SQL (Postgres, SQLite, MySQL, Oracle DB)
-- **Frameworks/Libraries:** BeautifulSoup, Scrapy, Flask, Pandas, Selenium
+- **Languages & Databases:** Python, Golang, SQL (Postgres, SQLite, MySQL), NoSQL (MongoDB)
+- **Frameworks & Libraries:** Langchain, Gin, BeautifulSoup, Scrapy, Flask, Selenium, Streamlit
+- **Cloud & DevOps:** AWS (EC2, RDS, S3, IAM), GCP (Compute, Cloud SQL, Cloud Run), Docker, Linux
 - **Version Control:** Git, GitHub
-- **Cloud and DevOps:** AWS EC2, RDS, S3, Elastic Beanstalk, EBS, Linux (Configuring and Managing), Docker
-- **Others:** Object-Oriented Programming (OOP), Functional Programming, Networking, Databases, ETL, CI/CD Pipelines, Agile
+- **Others:** OOP, API Development, AI-Powered Tools, Web Scraping, CRUD API Development with Go
 """)
 
 # Experience
-st.header('Experience')
+st.header('PROFESSIONAL EXPERIENCE')
 st.write("""
 **SDE 1 | Wissen Research**  
 *January 2024 – Present*  
-- Architected and implemented advanced web scrapers that performed comprehensive scans of hundreds of websites, significantly enhancing the efficiency and scope of data collection.
-- Led the development of a sophisticated web application and various APIs that consolidated data from diverse sources, providing real-time updates and robust data management capabilities.
-- Collaborated with a team of seasoned developers to establish cloud operations with AWS and build CI/CD pipelines, playing a key role in the development and deployment of the web application.
+- Developed web scrapers for 100+ websites, boosting data collection efficiency by 40%.
+- Led the creation of a centralized web app for real-time data management, significantly enhancing decision-making processes.
+- Designed CI/CD pipelines with GitLab and AWS, ensuring secure and scalable deployments.
+- Integrated CRUD APIs using Python and Go, improving backend functionality and streamlining database interactions.
 
-**Technologies used:** AWS, Python, Flask, HTML, Javascript, MySQL, Scrapy, Selenium, BeautifulSoup, JSON, Postman, SQLite, API, Docker, Linux
+**Tech Stack:** Python, Golang, Flask, MySQL, Scrapy, Selenium, BeautifulSoup, Docker, Postman, AWS, GCP, API, SQLite, Go
 """)
 
 # Education
-st.header('Education')
+st.header('EDUCATION')
 st.write("""
-**Modern College of Arts, Science and Commerce (Autonomous), Pune**  
+**Modern College of Arts, Science, and Commerce (Autonomous), Pune**  
 *2021 - 2024*  
 - **Bachelor of Computer Applications (Science)**  
-- **SGPA:** 8.7
+- **CGPA:** 8.9
 
-**Relevant Coursework:** Object Oriented Programming, Databases, Discrete Maths, Data Structures and Algorithms, Operating Systems, Computer Networks, Web Development, Software Development, Cloud Computing(AWS)
+**Relevant Coursework:** OOP, Databases, SDLC, Discrete Math, Data Structures & Algorithms, Operating Systems, Computer Networks, Web Development, Cloud Computing (AWS)
 """)
 
 # Projects
-st.header('Projects')
+st.header('PROJECTS')
 st.write("""
+**AI-Powered Web Scraper**  
+*May 2024*  
+- Developed an AI-powered web scraper using Llama 3.1 and Ollama, extracting and analyzing data from multiple platforms.
+- Integrated Selenium for automated interactions and deployed on AWS for scalable usage, doubling data extraction efficiency.
+
 **Algorithmic Trading Bot**  
 *November 2023*  
-- Developed an algorithmic trading bot using Python to automate and optimize my trading strategy.
-- Leveraged APIs to fetch real-time stock data and implemented strategy with processes to automate strike price selections, taking multiple trades.
+- Built an algorithmic trading bot to automate stock trading, boosting transaction efficiency.
+- Integrated APIs to fetch real-time stock data and automate decision-making strategies.
 
-**SQL Injection Scanner**  
-*October 2023*  
-- Developed a SQL injection scanner to automate testing for forms on websites which could be vulnerable.
-- Decreased time spent on manual testing for SQL injections.
+**Infringement Analysis Tool**  
+*September 2024*  
+- Collaborated in a ML-based tool for infringement analysis, focusing on API development and deploying on GCP Cloud Run.
+- Deployed APIs to identify potential legal infringements with high accuracy.
 
-**Project Manager**  
-*March 2024*  
-- Developed a Project Manager using Flask for my organization to help manage projects by creating a proper hierarchy between different departments.
+**Project Manager Application**  
+*April 2024*  
+- Designed and implemented a project management tool, fostering cross-departmental collaboration, resulting in a 25% increase in organizational efficiency.
 
-**Web Scrapers**  
+**Web Scrapers Suite**  
 *January 2024*  
-- Developed web scrapers for my organization to handle diverse data collection needs, utilizing technologies like Scrapy, SQLite, AWS EC2, RDS, MySQL, HTML, JSON, and AWS S3.
-
-**PDF Scraper**  
-*January 2024*  
-- Created a PDF file scraper using Selenium, Scrapy, and OCR to overcome data scraping challenges from PDF files.
-- Doubled efficiency of projects by removing the manual data mining from PDF files.
+- Developed a suite of web scrapers that automated data collection from multiple platforms, reducing project turnaround times by 50%.
 """)
 
 # Certifications
-st.header('Certifications')
+st.header('CERTIFICATIONS')
 st.write("""
 - **AWS Academy Graduate** - AWS Academy Cloud Foundations
-- **Certified Ethical Hacker (CEH)** - Training Certification by Sysap Technologies
-- **CCNA (Cisco Certified Network Associate)** - Training Certification by Sysap Technologies
+- **MongoDB Python Developer Path** - MongoDB University
+- **Machine Learning for All** - University of London
+- **Prompt Engineering for Developers** - Infosys Springboard
 """)
 
 # Footer
 st.write("---")
 st.write("© 2024 Kuldeep Singh. All Rights Reserved.")
-
